6aebb69f2b25c59229a55f1c0d96a8ccc45f64a2
Tausal21 <salmanrefayet@gmail.com>
2019-12-20 03:00:40 -0500

    closest POI column added and average/stdev calculated

--- task.py
+++ task.py
@@ -2,6 +2,12 @@ import pandas as pd
 import numpy as np
 from math import *
 from scipy import spatial
+from statistics import stdev
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+
+%matplotlib inline
+plt.style.use('seaborn-poster')
 
 def to_Cartesian(lat, lng):
     '''
@@ -51,6 +57,8 @@ def kmToDIST(x):
     dist = 2 * R * rad2deg(sin(gamma / 2.))
     return (dist)
 
+def Average(lst):
+    return sum(lst) / len(lst)
 
 initialData = pd.read_csv('/Users/Tausal21/Desktop/EQ_Works/project/ws-data-spark/data/DataSample.csv')
 POIData = pd.read_csv('/Users/Tausal21/Desktop/EQ_Works/project/ws-data-spark/data/POIList.csv')
@@ -80,25 +88,39 @@ POI_coordinates = list(zip(POI_x, POI_y, POI_z))
 tree = spatial.cKDTree(POI_coordinates)
 
 
-i=0
-dist_list = []
-POI_index_list = []
-login_index_list = []
+POI_01 = [] #storing min distances from logins to POI_01
+POI_02 = [] #  " " " POI_02
+POI_03 = [] #  " " " POI_03
+POI_04 = [] #  " " " POI_04
+POI_list = [] #we append this list as a column at the end of the original filtered dataset
 for index, row in filteredData.iterrows():
     x_req, y_req, z_req = to_Cartesian(row['Latitude'], row['Longitude'])
     dist, POI_index = tree.query((x_req, y_req, z_req), 1)
 
-    login_index_list.append(row['_ID'])
-    dist_list.append(dist)
-    POI_index_list.append(POI_index)
+    POI_list.append(POI_index)
+
+    if(POI_index==0): POI_01.append(dist)
+    if(POI_index==1): POI_02.append(dist)
+    if(POI_index==2): POI_03.append(dist)
+    if(POI_index==3): POI_04.append(dist)
+
+    #i+=1
+    #if(i==3): break
+filteredData['Nearest_POI'] = POI_list
+print(filteredData.head())
+
+POI_01_avg_dist_requestPoints = Average(POI_01)
+POI_02_avg_dist_requestPoints = Average(POI_02)
+POI_03_avg_dist_requestPoints = Average(POI_03)
+POI_04_avg_dist_requestPoints = Average(POI_04)
+
+POI_01_stdev_requestPoints = stdev(POI_01)
+POI_02_stdev_requestPoints = stdev(POI_02)
+POI_03_stdev_requestPoints = stdev(POI_03)
+POI_04_stdev_requestPoints = stdev(POI_04)
 
 
-    i+=1
-    if(i==3): break
 
-login_and_dist_list = list(zip(login_index_list, POI_index_list, dist_list))
-
-print(login_and_dist_list)
 
 
 
